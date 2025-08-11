e4db0cdfe256c13c41885c9869d973ef9ecb6b53
Jatin Pal <paljatin681@gmail.com>
2025-08-11 22:28:26 +0530

    first component created

---
+++ src/components/Body.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Body = () => {
+  return (
+    <div>
+      <h1>Main Content</h1>
+      <h3>This is the main content</h3>
+    </div>
+  )
+}
+
+export default Body;
---
+++ src/components/Greet.jsx
@@ -0,0 +1,6 @@
+const Greet = () => {
+
+    return <h1>Hello World</h1>
+};
+
+export default Greet;
