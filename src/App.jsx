ee63cded271b8ddf2494e128a0250a7959aa7aaa
Jatin Pal <paljatin681@gmail.com>
2025-08-12 22:58:38 +0530

    Update files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 import Greet from "./components/Greet";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import Footer from "./components/Footer";
+import Map from "./components/Map";
+import Password from "./components/Password";
 
 const App = () => {
   return <div>
     <Greet />
     <Header />
     <Body />
+    <Footer />
+    <Map />
+    <Password isValid={false } />
+    <User name = "Jatin PAl" age = {21} isMarried = {false} hobbies = {["coding", "Gaming", "Sleeping"]}/>
   </div>
+
 };
+
+
+const User = (props) => {
+  return <section>
+    <h1>Name: {props.name}</h1>
+    <h1>age: {props.age}</h1>
+    <h1>isMarriage: {props.isMarried}</h1>
+    <h1>Hobbies: {props.hobbies}</h1>
+  </section>
+};
+
 
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      ©2025 My Website
+    </div>
+  )
+}
+
+export default Footer;
